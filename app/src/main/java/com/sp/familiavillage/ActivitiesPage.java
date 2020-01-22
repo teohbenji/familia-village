09009a421fafa316db525c6a5c2fbf0114dd50e1
benjamin <teohbenjamin1@gmail.com>
2020-01-22 14:59:17 +0800

    First commit

---
+++ app/src/main/java/com/sp/familiavillage/ActivitiesPage.java
@@ -0,0 +1,13 @@
+package com.sp.familiavillage;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ActivitiesPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
