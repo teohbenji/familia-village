878d84674f1fb4ca8e822cb2815921e1a985e6f7
benjamin <teohbenjamin1@gmail.com>
2020-01-31 14:42:11 +0800

    Link buttons from HomePage to QRCodePage and vice versa

--- app/src/main/java/com/sp/familiavillage/HomePage.java
+++ app/src/main/java/com/sp/familiavillage/HomePage.java
@@ -1,6 +1,9 @@
 package com.sp.familiavillage;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
 
@@ -8,5 +11,17 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        findViewById(R.id.QR_scanner).setOnClickListener(onQR);
     }
+
+    public View.OnClickListener onQR =  new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            Intent intent;
+            intent = new Intent(HomePage.this, QRCodePage.class);    //returns to home page
+            startActivity(intent);
+        }
+    };
+
 }
