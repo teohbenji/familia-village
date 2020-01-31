d200ecc424ce5fff5f51f398daff96086d1f7354
benjamin <teohbenjamin1@gmail.com>
2020-01-31 16:03:23 +0800

    Linked RewardsPage with HomePage

--- app/src/main/java/com/sp/familiavillage/HomePage.java
+++ app/src/main/java/com/sp/familiavillage/HomePage.java
@@ -3,7 +3,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
 
@@ -11,8 +10,19 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        findViewById(R.id.awards).setOnClickListener(onRewards);
         findViewById(R.id.QR_scanner).setOnClickListener(onQR);
     }
+
+    public View.OnClickListener onRewards =  new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            Intent intent;
+            intent = new Intent(HomePage.this, RewardsPage.class);
+            startActivity(intent);
+        }
+    };
 
     public View.OnClickListener onQR =  new View.OnClickListener() {
 
